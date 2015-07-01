7521ea1e1ac7a46a55f1233cdb947ea46ae23edd
jtmclucas13 <jtmclucas13@gmail.com>
2015-07-01 01:14:31 -0400

    finish homepage

--- scripts/nav-control.js
+++ scripts/nav-control.js
@@ -1,5 +1,6 @@
 ---
 ---
+/* This makes the nav appear when clicking "Menu" */
 $("#menu-icon").click(function(event){
 	event.preventDefault();
 	if( $(this).hasClass("activeNav") ){
@@ -12,6 +13,18 @@ $("#menu-icon").click(function(event){
 	return false;
 });
 
-/*$(window).resize(function(){
-	$(".homepage-heading").css("font-size", $(window).width() / 100);
-});*/
+/* This enables smooth scrolling */
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
